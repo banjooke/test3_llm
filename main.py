e66a8798347e92d14ed43a2d5ac5edf852945b30
banjooke <banjooke@yahoo.com>
2024-11-11 19:15:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain import PromptTemplate, LLMChain
+from langchain_core.prompts import ChatPromptTemplate
 
 openai_api_key = st.secrets["openai"]["OPENAI_API_KEY"]
 
 llm = OpenAI(api_key=openai_api_key)
-template = "You are an experienced scientist and you write scientific blog {topic} only. Do not write anything that is not scientifically related"
-prompt = PromptTemplate(template=template, input_variables = ["topic"] )
-llm_chain=LLMChain(prompt=prompt, llm=llm)
+template = ChatPromptTemplate.from_message (
+    [
+       (
+           "system", "You are a Chemistry Teacher, you only address chemistry related questions"
+       ) ,
+        (
+            "user","{topic}"
+        )
+    ]
+)
+prompt = template.format_messages(input_variables = ["topic"] )
+)
 
 st.title("Scientific Blog App with LangChain")
 user_topic = st.text_input("WHich topic do you want to write about?")
 
 if st.button("Generate Contents"):
     if user_topic:
-        response = llm_chain.run(topic=user_topic, max_tokens=2000)
+        response = llm.invoke(prompt, max_tokens=2000)
         st.write(response)
     else:
         st.write("Please enter a topic")
